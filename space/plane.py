723fc4a30a3133fc728cc421def9836d02b89872
Jadi Josh <jadijosh@Jadis-MBP.home>
2020-10-18 20:03:59 -0400

    progress on linear packing, dispensed with basis vectors

--- space/plane.py
+++ space/plane.py
@@ -3,6 +3,7 @@ This class represents a finite plane.
 """
 
 import numpy as np
+import math
 from SpherePacker import Space
 
 class Plane(Space):
@@ -14,39 +15,26 @@ class Plane(Space):
 		describing the space
 		"""
 
-		super().__init__(space_id,space_dimensions);
-		"""
-		a set of unit vectors describing the tilt between the center of the circles in layer i and layer i+1, 
-		these values simplify coordinate resolution during rendering; the x-axis basis vector will always be <1,0>
-		"""
-		self.y_basis_vectors = [];	 
-
-		# construct grid
-		self.grid = np.zeros(space_dimensions);
+		super().__init__(space_id,space_dimensions);	 
 
 		# create empty array of circles
 		circles = [];
 
 	def insert_circle(self, sphere, pos):
-		# add circle to record
+		# add circle
 		circles.append(sphere);
-		
-		# populate grid cell at pos_x,pos_y
-		pos_x = pos[0];
-		pos_y = pos[1];
-		self.grid[pos_x,pos_y] = 1;
 
 		return True;
 
 
 	def pack_space(self, r=randrange(10), initial_pos=None, lattice_type):
 		# verify that initial_pos is valid
-		if(initial_pos[0] > self.space_dimensions[0] or initial_pos[1] > self.space_dimensions[1]):
+		if initial_pos[0] > self.space_dimensions[0] or initial_pos[1] > self.space_dimensions[1]:
 			return False;
 
-		if(lattice_type == 'linear')
-			self._pack_linearly(r,initial_pos);
-		elif(lattice_type == 'hexagonal'):
+		if lattice_type == 'linear':
+			self._pack_linearly(r, initial_pos);
+		elif lattice_type == 'hexagonal':
 			self._pack_hexagonaly(self, r, initial_pos);
 
 
@@ -55,25 +43,56 @@ class Plane(Space):
 	def _pack_linearly(self, r, initial_pos):
 		if(initial_pos == None):
 			# set in bottom left corner
-			initial_pos = (r,r);
+			initial_pos = (r, r);
 		
 		# insert a circle at initial_pos
-		self.insert_circle(Circle(r,initial_pos));
-		# the y basis vector of the initial coordinate system
-		# will be <0,1>
-		self.y_basis_vectors.append((0,1));
+		self.insert_circle(Circle(r, initial_pos));
 
 		x_max = space_dimensions[0];
 		y_max = space_dimensions[1];
+		x_cur = r;
+		y_cur = r;
+		row_cur = range(r, x_max, 2*r);
 		# pack exhaustively to the right
-		for x in range(r, x_max, 2*r):
-			# next circle will not overflow
-			pos = (x + 2*r,r)
-			insert_circle()
+		for x _cur in row_cur:
+			
+			# if edge of next circle is out of bounds on the right and packing is going from left to right
+			# shift it onto next layer (update y_cur,x_cur), compute basis vector, modify row_cur
+			if x_cur + 3 * r > x_max and row_cur[-1] == x_max:
+				overflow_distance = (x_cur + 3*r - x_max)
+				
+				# save x_cur, y_cur
+				x_old = x_cur;
+				y_old = y_cur;
+				
+				# shift circle onto next layer
+				x_cur = x_max - r;
+				y_cur = y_cur + math.sqrt(4 * (r ** 2)  - overflow_distance ** 2); # see the README for a derivation
+				
+				# modify row_cur, now packing is right to left
+				row_cur = range(x_cur, 0, -2*r);
 
-		
-			# keep resetting x until y_max is reached
-		pass
+			# if edge of next circle is out of bounds on the left and packing is going from right to left
+			# shift it onto next layer (update y_cur,x_cur), compute basis vector, modify row_cur
+			elif x_cur - 3 * r < 0 and row_cur[-1] == 0:
+				overflow_distance = -(x_cur - 3 * r);
+
+				# save x_cur, y_cur
+				x_old = x_cur;
+				y_old = y_cur;
+
+				# shift circle onto next layer
+				x_cur = r;
+				y_cur = y_cur + math.sqrt(4 * (r ** 2)  - overflow_distance ** 2);
+
+				# modify row_cur, now packing is left to right
+				row_cur = range(r, x_max, 2*r);				
+
+			# insert circle
+			pos_cur = (x_cur,y_cur);
+			if y_cur >= y_max:
+				break;
+			insert_circle(Circle(r,pos_cur));
 
 	def _pack_hexagonaly(self, r, initial_pos):
 		if(initial_post == None):
