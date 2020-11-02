97cd47904fa85d262ccaaa4f65096766b0cfd96b
Jadi Josh <jadijosh@Jadis-MBP.home>
2020-11-01 22:40:54 -0500

    edited _pack_upwards

--- space/plane.py
+++ space/plane.py
@@ -1,8 +1,6 @@
 """
 This class represents a finite plane.
 """
-
-import numpy as np
 import math
 from SpherePacker import Space
 
@@ -18,11 +16,11 @@ class Plane(Space):
 		super().__init__(space_id,space_dimensions);	 
 
 		# create empty array of circles
-		circles = [];
+		self.circles = [];
 
 	def insert_circle(self, sphere):
 		# add circle
-		circles.append(sphere);
+		self.circles.append(sphere);
 
 		return True;
 
@@ -40,38 +38,30 @@ class Plane(Space):
 
 		return True;
 
-	def _pack_linearly(self, r, initial_pos):
-		if(initial_pos == None):
-			# set in bottom left corner
-			initial_pos = (r, r);
+	def _pack_linearly(self, r, initial_pos=(r,r)):
 		
 		# insert a circle at initial_pos
 		self.insert_circle(Circle(r, initial_pos));
 
-		x_max = self.space_dimensions[0];
-		y_max = self.space_dimensions[1];
-		x_cur = initial_pos[0];
-		y_cur = initial_pos[1];
-		row_cur = range(r, x_max, 2*r);
 		# pack upwards
-		_linear_upwards(initial_pos);
+		self._linear_upwards(initial_pos);
 		# pack downwards
-		_linear_downwards(initial_pos);
+		self._linear_downwards(initial_pos);
 
-	def _linear_upwards(initial_pos):
+	def _linear_upwards(self, r, initial_pos):
 
 		x_max = self.space_dimensions[0];
 		y_max = self.space_dimensions[1];
-		x_cur = initial_pos[0];
-		y_cur = initial_pos[1];
-		row_cur = range(r, x_max, 2*r);
-		# pack
+
+		# start packing from right to left
+		# the endpoint for range is x_max + 2 * r so that x_max is included in the sequence
+		row_cur = range(initial_pos[0], x_max + 2 * r, 2 * r);
 		for x _cur in row_cur:
 			
 			# if edge of next circle is out of bounds on the right and packing is going from left to right
-			# shift it onto next layer (update y_cur,x_cur), compute basis vector, modify row_cur
+			# shift it onto next layer (update y_cur,x_cur), modify row_cur to go from right to left
 			if x_cur + 3 * r > x_max and row_cur[-1] == x_max:
-				overflow_distance = (x_cur + 3*r - x_max)
+				overflow_distance = (x_cur + 3 * r) - x_max;
 				
 				# save x_cur, y_cur
 				x_old = x_cur;
@@ -82,7 +72,7 @@ class Plane(Space):
 				y_cur = y_cur + math.sqrt(4 * (r ** 2)  - overflow_distance ** 2); # see the README for a derivation
 				
 				# modify row_cur, now packing is right to left
-				row_cur = range(x_cur, 0, -2*r);
+				row_cur = range(x_cur, -2 * r, -2 * r);
 
 			# if edge of next circle is out of bounds on the left and packing is going from right to left
 			# shift it onto next layer (update y_cur,x_cur), compute basis vector, modify row_cur
@@ -100,18 +90,19 @@ class Plane(Space):
 				# modify row_cur, now packing is left to right
 				row_cur = range(r, x_max, 2*r);				
 
-			# insert circle
-			pos_cur = (x_cur,y_cur);
-			if y_cur >= y_max:
+			# if there is space to shift upwards, quit
+			if y_cur + r > y_max:
 				break;
+			# otherwise, insert circle
+			pos_cur = (x_cur,y_cur);
 			insert_circle(Circle(r,pos_cur));
 
-	def _linear_downwards:
+	def _linear_downwards(self, r, initial_pos, r):
 
 		pass
 
 	def _pack_hexagonaly(self, r, initial_pos):
-		if(initial_post == None):
+		if(initial_pos == None):
 			# set in center
 			pass
 
